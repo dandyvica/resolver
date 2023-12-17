ea2aafc1d162cbd8058253c3a77f9a31bab01c4a
dandyvica <dandyvica@gmail.com>
2023-12-17 18:04:35 +0100

    moved to ResolverList struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-use std::net::IpAddr;
-
 pub mod error;
 
 #[cfg(target_family = "unix")]
@@ -8,9 +6,3 @@ pub mod unix;
 //pub mod resolver;
 #[cfg(target_family = "windows")]
 pub mod win;
-
-#[derive(Debug)]
-pub struct Resolvers {
-    pub v4: Vec<IpAddr>,
-    pub v6: Vec<IpAddr>,
-}
--- src/unix.rs
+++ src/unix.rs
@@ -1,34 +1,59 @@
 use crate::error::Error;
 use std::net::IpAddr;
 
+use std::path::Path;
 use std::str::FromStr;
 
-impl Resolvers {
+#[derive(Debug)]
+pub struct ResolverList {
+    resolvers: Vec<IpAddr>,
+}
+
+impl ResolverList {
+    pub fn len(&self) -> usize {
+        self.resolvers.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.resolvers.is_empty()
+    } 
+
+    // get the list of all resolvers
+    pub fn to_ip_vec(&self) -> &[IpAddr] {
+        &self.resolvers
+    }       
+}
+
+impl ResolverList {
     /// Return IPV4 & IPV6 DNS resolvers on the machine.
-    pub fn get_servers(conf: Option<&str>) -> Result<Self, Error> {
+    pub fn get() -> Result<Self, Error> {
         const RESOLV_CONF_FILE: &str = "/etc/resolv.conf";
 
-        // resolv file is usually at "/etc/resolv.conf" but some distros (Ubuntu) moved it elsewhere
-        let resolv_file = conf.unwrap_or(RESOLV_CONF_FILE);
+        let path = Path::new(RESOLV_CONF_FILE);
+        ResolverList::try_from(path)
+    }
+}
 
+// TryFrom will be used to build the DNS servers' list from a resolve.conf-like file
+impl TryFrom<&Path> for ResolverList {
+    type Error = Error;
+
+    fn try_from(resolv_file: &Path) -> Result<Self, Self::Error> {
         // read whole file, get rid of comments and extract DNS stubs
         let resolv_conf = std::fs::read_to_string(resolv_file)?;
 
-        let servers: Vec<IpAddr> = resolv_conf
+        let resolvers: Vec<IpAddr> = resolv_conf
             .lines()
             .filter(|line| line.trim().starts_with("nameserver"))
             .filter_map(|addr| addr.split_ascii_whitespace().nth(1))
             .map(IpAddr::from_str)
             .collect::<Result<Vec<_>, _>>()?;
 
-        if servers.is_empty() {
+        if resolvers.is_empty() {
             return Err(Error::NoResolverConfigured);
         }
 
-        let v4: Vec<IpAddr> = servers.iter().filter(|x| x.is_ipv4()).cloned().collect();
-        let v6: Vec<IpAddr> = servers.iter().filter(|x| x.is_ipv6()).cloned().collect();
-
-        Ok(Self { v4, v6 })
+        Ok(Self { resolvers })
     }
 }
 
@@ -38,20 +63,19 @@ mod tests {
     use std::net::IpAddr;
     use std::str::FromStr;
 
-    use crate::resolver::Resolvers;
+    use super::*;
 
     #[test]
     fn test_unix() {
-        let s1 = Resolvers::get_servers(Some("./tests/resolv.conf"));
-        assert!(s1.is_ok());
+        let list = ResolverList::try_from(Path::new("./tests/resolv.conf"));
+        assert!(list.is_ok());
 
-        let s2 = s1.unwrap();
-        assert_eq!(s2.v4.len(), 2);
-        assert_eq!(s2.v6.len(), 2);
+        let list = list.unwrap();
+        assert_eq!(list.len(), 4);
 
-        assert!(s2.v4.contains(&IpAddr::from_str("45.90.28.55").unwrap()));
-        assert!(s2.v4.contains(&IpAddr::from_str("45.90.30.55").unwrap()));
-        assert!(s2.v6.contains(&IpAddr::from_str("2a07:a8c0::").unwrap()));
-        assert!(s2.v6.contains(&IpAddr::from_str("2a07:a8c1::").unwrap()));
+        assert!(list.resolvers.contains(&IpAddr::from_str("45.90.28.55").unwrap()));
+        assert!(list.resolvers.contains(&IpAddr::from_str("45.90.30.55").unwrap()));
+        assert!(list.resolvers.contains(&IpAddr::from_str("2a07:a8c0::").unwrap()));
+        assert!(list.resolvers.contains(&IpAddr::from_str("2a07:a8c1::").unwrap()));
     }
 }
